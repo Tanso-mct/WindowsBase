1812f6d069ff218e0e924ab01c4c79dda1628c8c
Tanso <tansomctexp@gmail.com>
2025-07-09 17:19:13 +0900

    Moved ConsoleLog.

--- windows_base/windows_base/src/pch.h
+++ windows_base/windows_base/src/pch.h
@@ -1,4 +1,11 @@
 ﻿#pragma once
 
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
 #include <unordered_map>
 #include <memory>
+#include <initializer_list>
+#include <string>
+#include <string_view>
+#include <iostream>
