e647e14b36f110f427ac8656ff590f0e23393711
Tanso <tansomctexp@gmail.com>
2025-07-09 16:19:37 +0900

    PCH及びDLL_CONFIGの作成。

---
+++ windows_base/windows_base/include/dll_config.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#define BUILDING_WINDOWS_BASE_DLL
+#ifdef BUILDING_WINDOWS_BASE_DLL
+#define WINDOWS_BASE_API __declspec(dllexport)
+#else
+#define WINDOWS_BASE_API __declspec(dllimport)
+#endif
