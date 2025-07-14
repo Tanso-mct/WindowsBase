568bb45d875785471550ed159575c87bfd2d7ce2
Tanso <tansomctexp@gmail.com>
2025-07-14 10:47:58 +0900

    Creating sccene test

---
+++ windows_base/windows_base_test/scene_test.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+
+#include "windows_base/include/scene.h"
+#pragma comment(lib, "windows_base.lib")
+
+namespace
+{
+    class MockEntitiesFactory : public wb::IEntitiesFactory
+    {
+    public:
+        void Create
+        (
+            wb::IAssetContainer &assetCont, wb::IEntityContainer &entityCont,
+            wb::IComponentContainer &componentCont, wb::IEntityIDView &entityIDView
+        ) const override
+        {
+            // Mock implementation
+        }
+    };
+}
+
+TEST(Scene, Create)
+{
+    std::unique_ptr<wb::ISceneFacade> sceneFacade = std::make_unique<wb::SceneFacade>();
+    {
+        std::unique_ptr<wb::ISceneContext> sceneContext = std::make_unique<wb::SceneContext>();
+        sceneFacade->SetContext(std::move(sceneContext));
+    }
+}
