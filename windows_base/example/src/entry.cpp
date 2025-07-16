75ff428c6be2523ad6d57e7b271125d6c4473580
Tanso <tansomctexp@gmail.com>
2025-07-16 19:14:01 +0900

    Setting example project.

---
+++ windows_base/example/src/entry.cpp
@@ -0,0 +1,38 @@
+﻿#include "example/src/pch.h"
+
+#pragma comment(lib, "windows_base.lib")
+
+static LRESULT CALLBACK WindowProcWithEvent(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    wb::WindowsBaseLibrary::HandleWindowProc(hWnd, msg, wParam, lParam);
+
+    switch (msg)
+    {
+    case WM_PAINT:
+        break;
+
+    default:
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+int APIENTRY wWinMain
+(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nCmdShow
+){
+    wb::LibraryConfig config;
+    config.windowProc_ = WindowProcWithEvent;
+
+    // Initialize the Windows Base Library
+    wb::WindowsBaseLibrary::Initialize(config);
+
+    // Run the library which will start the message loop
+    wb::WindowsBaseLibrary::Run();
+
+    return 0;
+}
